6c16a8b66f61111f32bca8829cf36c49c4cd4afa
Jino Bae <im@baejino.com>
2025-04-08 18:07:40 +0900

    Refactor: UUID 생성 방식 수정

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,13 @@
 import '@testing-library/jest-dom';
 
+import crypto from 'crypto';
+
+Object.defineProperty(window, 'crypto', {
+  value: {
+    randomUUID: crypto.randomUUID,
+  },
+});
+
 export function mockRequestAnimationFrame() {
   window.requestAnimationFrame = (callback) => {
     callback(0);
--- src/modules/uuid.ts
+++ src/modules/uuid.ts
@@ -1,7 +1,3 @@
-export function createUUID(): string {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0,
-      v = c == 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
+export const createUUID = () => {
+  return crypto.randomUUID() as string;
+};
