232f12f1825281941a626677c8ddbb70e8d13ae2
Jino Bae <im@baejino.com>
2025-04-09 14:31:01 +0900

    Style: paths to module name mapper 적용

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,12 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '~/(.*)$': '<rootDir>/src/$1',
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' }),
   },
   setupFilesAfterEnv: ['./jest.setup.ts'],
 };
